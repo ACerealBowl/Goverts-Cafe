7e4a4a5137d98718ec34a61b8be359594b51d7d9
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-19 18:20:25 +0300

    spelling again

--- Assets/Scripts/Main Menu/GLogo.cs
+++ Assets/Scripts/Main Menu/GLogo.cs
@@ -1,23 +1,25 @@
 using UnityEngine;
-
+using System.Collections;
+using System.Collections.Generic;
 public class LogoMainMenu : MonoBehaviour
 {
-public Animator PersonAnimator;
-public string  animationName = "LogoMainMenu";
-private void Start()
-{
-personAnimator = GetComponent<Animator>();
-StartCoroutine(PlayAnimation());
-}
+    public Animator personAnimator;
+    public string animationName = "LogoMainMenu";
+    private void Start()
+    {
+        personAnimator = GetComponent<Animator>();
+        StartCoroutine(PlayAnimation());
+    }
 
-private IEnumarator
-{
-if(personAnimator.HasState(0, Animator.StringToHash(animationName)))
-{
-yield return new WaitForSeconds(8f);
-personAnimator.Play(animationName, 0);
-yield return new WairForSeconds(personAnimator.GetCurrentAnimatorStateInfo(0).lenght);
+    private IEnumerator PlayAnimation()
+    {
+        if (personAnimator.HasState(0, Animator.StringToHash(animationName)))
+        {
+            yield return new WaitForSeconds(8f);
+            personAnimator.Play(animationName, 0);
+            yield return new WaitForSeconds(personAnimator.GetCurrentAnimatorStateInfo(0).length);
+        }
+    }
 }
-
 
 
