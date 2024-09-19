0b76f3be1b5416aa9293e8f3416ddc684eb70235
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-19 13:32:06 +0300

    Update GLogo.cs

--- Assets/Scripts/Main Menu/GLogo.cs
+++ Assets/Scripts/Main Menu/GLogo.cs
@@ -4,7 +4,20 @@ public class LogoMainMenu : MonoBehaviour
 
 public Animator PersonAnimator;
 public string  animationName = "LogoMainMenu";
+private void Start()
+{
+personAnimator = GetComponent<Animator>();
+StartCoroutine(PlayAnimation());
+}
 
+private IEnumarator
+{
+if(personAnimator.HasState(0, Animator.StringToHash(animationName)))
+{
+yield return new WaitForSeconds(8f);
+personAnimator.Play(animationName, 0);
+yeld return new WairForSeconds(personAnimator.GetCurrentAnimatorStateInfo(0).lenght);
+ 
 
 
 
