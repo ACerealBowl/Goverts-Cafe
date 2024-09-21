f6a91fa12d6ef0f0dae4a20997ba37c1f9f28917
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-21 12:32:54 +0300

    Mediocre UI Sound Effects

--- Assets/Scripts/Main Menu/AudioManager.cs
+++ Assets/Scripts/Main Menu/AudioManager.cs
@@ -3,22 +3,39 @@ using UnityEngine;
 public class AudioManager : MonoBehaviour
 {
     [Header("---balls---")]
-    [SerializeField]  AudioSource musicSource;
-    [SerializeField]  AudioSource SFXSource;
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource SFXSource;
     [Header("--clips---")]
     public AudioClip background;
     public AudioClip step;
+    public AudioClip uiHoverSound;  // added for UI hover and click
+    public AudioClip uiClickSound;
+
     private void Start()
     {
         musicSource.clip = background;
         musicSource.Play();
     }
+
     public void PlaySFX(AudioClip clip)
     {
         SFXSource.PlayOneShot(clip);
     }
+
     public void StopSFX()
     {
         SFXSource.Stop();
+    }
+
+    // New method for UI hover sound
+    public void PlayUIHoverSound()
+    {
+        PlaySFX(uiHoverSound);
+    }
+
+    // New method for UI click sound
+    public void PlayUIClickSound()
+    {
+        PlaySFX(uiClickSound);
     }
 }
