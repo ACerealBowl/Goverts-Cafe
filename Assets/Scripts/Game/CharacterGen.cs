e8b9d2055cc414d7499fd3bb60bac0cd94e376a4
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-25 11:04:49 +0300

    made it random - er

--- Assets/Scripts/Game/CharacterGen.cs
+++ Assets/Scripts/Game/CharacterGen.cs
@@ -3,17 +3,20 @@ using System.Collections;
 
 public class CharacterGen : MonoBehaviour
 {
-    public float min = 0f;  // to test what the average waiting time would be
-    public float max = 11f; 
+    public float min = 1;  // to test what the average waiting time would be
+    public float max = 20; 
     int dejavu = 0;
+    int dejavu2 = 0;
+    int dejavu3 = 0;
     int randomNumber = 1;
     void Update()
     {
-        while(randomNumber != dejavu)
+        while(randomNumber != dejavu && != dejavu2 && != dejavu3)
         {
            randomNumber = Random.Range((int)min, (int)max + 1); // yes this looks like it makes sense
            dejavu = randomNumber;
         }
+
     }
     
     
