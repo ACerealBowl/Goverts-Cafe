0e0092d044c13e3a915de9e9b4a7ac9170012fc3
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-25 09:58:45 +0300

    update Charactergen w mid code

--- Assets/Scripts/Game/CharacterGen.cs
+++ Assets/Scripts/Game/CharacterGen.cs
@@ -1,1 +1,20 @@
-//balls 
+using UnityEngine;
+using System.Collections;   
+
+public class CharacterGen : MonoBehaviour
+{
+    public float min = 0f;  // to test what the average waiting time would be
+    public float max = 11f; 
+    int dejavu = 0;
+    int randomNumber = 1;
+    void Update()
+    {
+        while(randomNumber != dejavu)
+        {
+           randomNumber = Random.Range((int)min, (int)max + 1); // yes this looks like it makes sense
+           dejavu = randomNumber;
+        }
+    }
+    
+    
+}
