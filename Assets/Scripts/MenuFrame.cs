35a42f53993b92e83ad20b94bb5c5cc95ed15db6
ACerealBowl <yourfellowthomas@gmail.com>
2024-09-26 21:05:08 +0300

    added fading twice

--- Assets/Scripts/MenuFrame.cs
+++ Assets/Scripts/MenuFrame.cs
@@ -5,13 +5,71 @@ using UnityEngine.SceneManagement;
 
 public class MenuFrame : MonoBehaviour
 {
+    public float fadeDuration = 1f;
+    public Color fadeColor = Color.black;
+    private Texture2D fadeTexture;
+    private float fadeAmount = 0f;
+    private bool isFading = false;
+    private bool Endler = false;
+
+    private void Start()
+    {
+        fadeTexture = new Texture2D(1, 1);
+        fadeTexture.SetPixel(0, 0, Color.white);
+        fadeTexture.Apply();
+    }
+
+    private void OnGUI()
+    {
+        if (fadeAmount > 0)
+        {
+            Color tempColor = GUI.color;
+            GUI.color = new Color(fadeColor.r, fadeColor.g, fadeColor.b, fadeAmount);
+            GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+            GUI.color = tempColor;
+        }
+    }
+
+    private void Update()
+    {
+        if (isFading)
+        {
+            fadeAmount = Mathf.MoveTowards(fadeAmount, targetFadeAmount, Time.deltaTime / fadeDuration);
+            if (fadeAmount == targetFadeAmount)
+            {
+                isFading = false;
+                if (fadeAmount == 1f)
+                {
+                    if (Endler)
+                    SceneManager.LoadSceneAsync("Endless");
+                }
+            }
+        }
+    }
+
+    private float targetFadeAmount;
+
+    private IEnumerator StartFading(float startAlpha, float targetAlpha)
+    {
+        fadeAmount = startAlpha;
+        targetFadeAmount = targetAlpha;
+        isFading = true;
+        yield return new WaitForSeconds(fadeDuration);
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(StartFading(0f, 1f));
+    }
+
     public void Story()
     {
         Application.Quit();
     }
     public void Endless()
     {
-        SceneManager.LoadSceneAsync("Endless");
+        Endler=true;
+        FadeOut();
     }
     public void Race()
     {
